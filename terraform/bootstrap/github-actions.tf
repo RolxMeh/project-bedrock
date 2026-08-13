222ada75d027b21252f991c41b655bca7ddd0e14
RolxMeh <ogeleayotunde@gmail.com>
2026-08-13 13:02:36 +0100

    updated iam trust policy

--- terraform/bootstrap/github-actions.tf
+++ terraform/bootstrap/github-actions.tf
@@ -40,7 +40,11 @@ resource "aws_iam_role" "github_actions" {
           }
 
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:RolxMeh@95518486/project-bedrock@1332954211:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:RolxMeh/project-bedrock:ref:refs/heads/main",
+              "repo:RolxMeh/project-bedrock:ref:refs/heads/dev",
+              "repo:RolxMeh/project-bedrock:pull_request"
+            ]
           }
         }
       }
