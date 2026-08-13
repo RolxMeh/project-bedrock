e4445f13ca06942ddeae8fb7b11ca669b3e328a6
RolxMeh <ogeleayotunde@gmail.com>
2026-08-13 12:19:46 +0100

    fix: update github actions oidc trust

--- terraform/bootstrap/github-actions.tf
+++ terraform/bootstrap/github-actions.tf
@@ -37,7 +37,8 @@ resource "aws_iam_role" "github_actions" {
           }
 
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:RolxMeh/project-bedrock:ref:refs/heads/main"          }
+            "token.actions.githubusercontent.com:sub" = "repo:RolxMeh@95518486/project-bedrock@1332954211:ref:refs/heads/main"
+          }
         }
       }
     ]
