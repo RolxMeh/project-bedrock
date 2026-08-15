f4d1d65215a44411459d43f94c5eb5400566c9d4
RolxMeh <ogeleayotunde@gmail.com>
2026-08-15 13:59:34 +0100

    Reverted

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -66,7 +66,7 @@ resource "aws_db_instance" "this" {
 
   publicly_accessible = false
 
-  backup_retention_period = 1
+  backup_retention_period = 7
   backup_window           = "03:00-04:00"
 
   deletion_protection = true
